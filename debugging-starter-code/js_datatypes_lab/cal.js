ee497b63089c75140450a5efc0957dc7c14de5bf
Hanad <hanad15@hotmail.co.uk>
2018-08-06 20:40:39 +0100

    calculator done

---
+++ debugging-starter-code/js_datatypes_lab/cal.js
@@ -0,0 +1,29 @@
+
+var num1 = prompt ("please enter first number");
+var operator = prompt ("please enter your operator")
+var num2 = prompt ("please enter second number");
+
+console.log (num1);
+console.log (operator);
+console.log (num2);
+
+a = parseInt(num1);
+b = parseInt(num2);
+
+
+if (operator == "+") {
+  num1 + num2
+  console.log (a+b);
+}
+if (operator == "-") {
+  num1 - num2
+  console.log (a-b);
+}
+if (operator == "*") {
+  num1 * num2
+  console.log (a*b);
+}
+if (operator == "/") {
+  num1 / num2
+  console.log (a/b);
+}
